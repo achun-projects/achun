640e44d8d9b6f50467132354bd1f6dc525791be3
Heiffeng <blacard@163.com>
2023-05-19 15:12:00 +0800

    change

--- achun-file/script/start-achun-file-app.sh
+++ achun-file/script/start-achun-file-app.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd /root/buckets/workspace/gitee/achun/achun-file
+cd /home/luokong/workspace/achun/achun-file
 git reset --hard origin/master
 mvn clean install -DskipTests
-cd /root/buckets/workspace/gitee/achun/achun-file/achun-file-app/target
+cd /home/luokong/workspace/achun/achun-file/achun-file-app/target
 
 java -jar achun-file-app.jar
