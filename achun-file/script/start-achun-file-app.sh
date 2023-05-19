cdb7f51b89d29de18587123c06ba950e34afac42
Heiffeng <blacard@163.com>
2023-05-19 14:56:48 +0800

    add script

---
+++ achun-file/script/start-achun-file-app.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd /root/buckets/workspace/gitee/achun/achun-file
+git reset --hard origin/master
+mvn clean install -DskipTests
+cd /root/buckets/workspace/gitee/achun/achun-file/achun-file-app/target
+
+java -jar achun-file-app.jar
