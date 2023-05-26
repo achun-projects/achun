686e0f6b17de6e77b37b9291ae821fb81db5e1bb
Heiffeng <blacard@163.com>
2023-05-26 14:23:02 +0800

    文件处理

--- achun-updown/achun-updown-app/src/main/java/site/achun/updown/app/controller/FileTransferController.java
+++ achun-updown/achun-updown-app/src/main/java/site/achun/updown/app/controller/FileTransferController.java
@@ -9,8 +9,11 @@ import site.achun.file.client.module.file.request.QueryByFileCode;
 import site.achun.file.client.module.file.response.FileInfoResponse;
 import site.achun.support.api.response.Rsp;
 import site.achun.updown.app.service.module.transfer.FileTransferInfo;
+import site.achun.updown.app.service.module.transfer.FileTransferService;
 import site.achun.updown.client.module.transfer.FileTransferClient;
 import site.achun.updown.client.module.transfer.request.RequestTransferFile;
+
+import java.nio.file.Path;
 
 @Slf4j
 @Tag(name = "文件处理", description = "文件处理")
@@ -20,16 +23,20 @@ public class FileTransferController implements FileTransferClient {
 
 
     private final FileQueryClient fileQueryClient;
+    private final FileTransferService fileTransferService;
 
     @Override
     public void transferFile(RequestTransferFile request) {
         var queryByFileCode = QueryByFileCode.builder()
                 .fileCode(request.getFileCode())
                 .build();
-        var fileResponse = fileQueryClient.queryFile(queryByFileCode).getData();
-
-        fileResponse
+        var fileResponse = fileQueryClient.queryFileLocalInfo(queryByFileCode).getData();
+        var localFilePath = Path.of(fileResponse.getStorage().getPath(), fileResponse.getInStoragePath());
         FileTransferInfo transfer = new FileTransferInfo();
-        transfer.setFile(new java.io.File(fileResponse.getFilePath()));
+        transfer.setFile(localFilePath.toFile());
+        transfer.setFileCode(fileResponse.getFileCode());
+        transfer.setStorage(fileResponse.getStorage());
+        transfer.setInStoragePath(fileResponse.getInStoragePath());
+        fileTransferService.transfer(transfer);
     }
 }
--- achun-updown/achun-updown-app/src/main/java/site/achun/updown/app/service/module/transfer/FileTransferService.java
+++ achun-updown/achun-updown-app/src/main/java/site/achun/updown/app/service/module/transfer/FileTransferService.java
@@ -2,9 +2,7 @@ package site.achun.updown.app.service.module.transfer;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import site.achun.file.client.module.file.response.InitFileInfoResponse;
 
 import java.util.Collection;
 import java.util.List;
@@ -16,27 +14,27 @@ import java.util.stream.Collectors;
 public class FileTransferService {
 
 
+    private final List<FileTransferStrategy> fileTransferStrategies;
     public void transfer(FileTransferInfo fileInfo){
-        // 判空
-//        transfer(fileTransferInfo,TransferType.ALL);
+        transfer(fileInfo,TransferType.ALL);
     }
 
     // 指定处理器
-//    public void transfer(FileTransferInfo fileTransferInfo, Collection<TransferType> handlerTypes){
-//        // 匹配执行器，进行操作
-//        List<FileTransferHandler> handlers = fileTransferHandlers.stream()
-//                .filter(handler -> handlerTypes.contains(handler.transferType()))
-//                .collect(Collectors.toList());
-//        for (FileTransferHandler fileTransferHandler : handlers) {
-//            if(fileTransferHandler.isHandler(fileTransferInfo)){
-//                try{
-//                    fileTransferHandler.handler(fileTransferInfo);
-//                }catch (Exception ex){
-//                    log.error("处理器发生异常",ex);
-//                    ex.printStackTrace();
-//                }
-//            }
-//        }
-//    }
+    public void transfer(FileTransferInfo fileTransferInfo, Collection<TransferType> types){
+        // 匹配执行器，进行操作
+        List<FileTransferStrategy> strategies = fileTransferStrategies.stream()
+                .filter(handler -> types.contains(handler.transferType()))
+                .collect(Collectors.toList());
+        for (FileTransferStrategy strategy : strategies) {
+            if(strategy.match(fileTransferInfo)){
+                try{
+                    strategy.handler(fileTransferInfo);
+                }catch (Exception ex){
+                    log.error("处理器发生异常",ex);
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
 
 }
