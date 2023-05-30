9c8ff13891bf03f4c2ec3cb5d209ab1317592864
Heiffeng <blacard@163.com>
2023-05-30 15:36:45 +0800

    Update FileUpdateListener.java

--- achun-gallery/achun-gallery-app/src/main/java/site/achun/gallery/app/listener/FileUpdateListener.java
+++ achun-gallery/achun-gallery-app/src/main/java/site/achun/gallery/app/listener/FileUpdateListener.java
@@ -1,16 +1,25 @@
 package site.achun.gallery.app.listener;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+import site.achun.file.client.module.file.FileQueryClient;
+import site.achun.file.client.module.file.request.QueryByFileCode;
+import site.achun.file.client.module.file.response.FileInfoResponse;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class FileUpdateListener {
 
+    private final FileQueryClient fileQueryClient;
+
     @RabbitListener(queues = "file.update.queue")
-    public void whenFileUpdate(String msg){
-        log.info("fileUpdate :{}",msg);
+    public void whenFileUpdate(String fileCode){
+        log.info("fileUpdate :{}",fileCode);
+        FileInfoResponse file = fileQueryClient.queryFile(QueryByFileCode.builder().fileCode(fileCode).build()).getData();
+        log.info("file:{}",file.getFileName());
     }
 
 }
