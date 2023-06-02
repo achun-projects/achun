9d8c88c777340ee73f5be9460c746b0aa710cfe5
Heiffeng <blacard@163.com>
2023-06-02 18:53:23 +0800

    Update AuthFilter.java

--- achun-gateway/src/main/java/site/achun/gateway/filter/AuthFilter.java
+++ achun-gateway/src/main/java/site/achun/gateway/filter/AuthFilter.java
@@ -34,7 +34,7 @@ public class AuthFilter implements GatewayFilter, Ordered {
 
     @Override
     public int getOrder() {
-        return Ordered.LOWEST_PRECEDENCE;
+        return Ordered.HIGHEST_PRECEDENCE;
     }
 }
 
