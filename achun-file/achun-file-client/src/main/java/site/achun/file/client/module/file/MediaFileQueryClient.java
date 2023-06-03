07bebac6c4fec9f1a9e6dc9ccf0ce1630e4a1243
Heiffeng <blacard@163.com>
2023-06-03 23:45:46 +0800

    Update MediaFileQueryClient.java

--- achun-file/achun-file-client/src/main/java/site/achun/file/client/module/file/MediaFileQueryClient.java
+++ achun-file/achun-file-client/src/main/java/site/achun/file/client/module/file/MediaFileQueryClient.java
@@ -14,24 +14,24 @@ import java.util.Map;
 @FeignClient(name = "achun-file-app", contextId = "MediaFileQueryClient")
 public interface MediaFileQueryClient {
     @Operation(summary = "根据文件唯一标识查询文件")
-    @PostMapping("/file/query/query-file-by-code")
+    @PostMapping("/file/query/query-media-file-by-code")
     Rsp<MediaFileResponse> queryFile(@RequestBody QueryByFileCode queryByFileCode);
     @Operation(summary = "根据文件唯一标识集合查询文件Map")
-    @PostMapping("/file/query/query-file-map-by-codes")
+    @PostMapping("/file/query/query-media-file-map-by-codes")
     Rsp<Map<String,MediaFileResponse>> queryFileMap(@RequestBody QueryByFileCodes queryByFileCodes);
     @Operation(summary = "根据单位唯一标识集合查询文件Map")
-    @PostMapping("/file/query/query-file-map-by-unit-codes")
+    @PostMapping("/file/query/query-media-file-map-by-unit-codes")
     Rsp<Map<String,MediaFileResponse>> queryFileMap(@RequestBody QueryByUnitCodes queryByUnitCodes);
     @Operation(summary = "根据单位唯一标识查询文件Map")
-    @PostMapping("/file/query/query-file-map-by-unit-code")
+    @PostMapping("/file/query/query-media-file-map-by-unit-code")
     Rsp<Map<String,MediaFileResponse>> queryFileMap(@RequestBody QueryByUnitCode queryByUnitCode);
     @Operation(summary = "根据文件唯一标识集合查询文件List")
-    @PostMapping("/file/query/query-file-list-by-codes")
+    @PostMapping("/file/query/query-media-file-list-by-codes")
     Rsp<List<MediaFileResponse>> queryFileList(@RequestBody QueryByFileCodes queryByFileCodes);
     @Operation(summary = "根据单位唯一标识查询文件List")
-    @PostMapping("/file/query/query-file-list-by-unit-code")
+    @PostMapping("/file/query/query-media-file-list-by-unit-code")
     Rsp<List<MediaFileResponse>> queryFileList(@RequestBody QueryByUnitCode queryByUnitCode);
     @Operation(summary = "根据单位唯一标识集合查询文件List")
-    @PostMapping("/file/query/query-file-list-by-unit-codes")
+    @PostMapping("/file/query/query-media-file-list-by-unit-codes")
     Rsp<List<MediaFileResponse>> queryFileList(@RequestBody QueryByUnitCodes queryByUnitCodes);
 }
