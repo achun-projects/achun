95f6358b6c7a2782290361b84f183618f92a08fb
Heiffeng <suna235792@gmail.com>
2023-06-05 18:44:14 +0800

    添加user项目

---
+++ achun-user/achun-user-app/src/main/java/com/example/achunuserapp/AchunUserAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.achunuserapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchunUserAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AchunUserAppApplication.class, args);
+    }
+
+}
---
+++ achun-user/achun-user-app/src/test/java/com/example/achunuserapp/AchunUserAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.achunuserapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchunUserAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
