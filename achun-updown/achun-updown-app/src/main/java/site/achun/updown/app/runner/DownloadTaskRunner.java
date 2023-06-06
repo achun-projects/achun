c6491df4f84679ebb3f05262fed295968486b31a
Heiffeng <suna235792@gmail.com>
2023-06-06 17:49:02 +0800

    异步下载

---
+++ achun-updown/achun-updown-app/src/main/java/site/achun/updown/app/runner/DownloadTaskRunner.java
@@ -0,0 +1,102 @@
+package site.achun.updown.app.runner;
+
+import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson2.JSONObject;
+import io.lettuce.core.RedisCommandTimeoutException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.redis.RedisConnectionFailureException;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import site.achun.file.client.module.download.request.Task;
+import site.achun.updown.app.service.module.download.DownloadTaskService;
+
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 异步文件下载启动器
+ *
+ * @Author: Heiffeng
+ * @Date: 2021/11/30 1:11
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DownloadTaskRunner implements CommandLineRunner {
+
+    @Value("${download.task.open}")
+    private Boolean open;
+
+    private final DownloadTaskService downloadTaskService;
+
+    private final StringRedisTemplate redisTemplate;
+
+    public static Boolean WAITING = false;
+
+    private final static ThreadPoolExecutor POOL = new ThreadPoolExecutor(
+            1,
+            2,
+            30,
+            TimeUnit.SECONDS,
+            new SynchronousQueue<>(),
+            (r, executor) ->{
+                try {
+                    executor.getQueue().put(r);
+                } catch (InterruptedException e) {e.printStackTrace();}
+            }
+    );
+
+    @Override
+    public void run(String... args) throws Exception {
+        // 异步下载开关状态判断
+        log.info("异步下载开关状态为：{}",open);
+        if(!open){
+            log.info("异步下载状态为关闭，不执行异步下载任务。");
+            return;
+        }
+        // 启动下载线程
+        Thread asynThread = new Thread(() -> {
+            while(true) {
+                try {
+                    if(WAITING){
+                        waiting(5);
+                    }
+                    // 从redis拉取队列数据
+                    String value = redisTemplate.opsForList().leftPop(Task.KEY, 10, TimeUnit.SECONDS);
+                    if (StrUtil.isEmpty(value)) {
+                        continue;
+                    }
+                    Task task = JSONObject.parseObject(value, Task.class);
+                    if (task == null) {
+                        log.info("无法转换，value:{}", value);
+                        continue;
+                    }
+                    POOL.execute(()->{
+                        downloadTaskService.handler(task);
+                    });
+                } catch (RedisCommandTimeoutException ex) {
+                    log.info("Redis等待超时……，进行下一次等待");
+                } catch (RedisConnectionFailureException ex){
+                    log.info("Redis链接失败……，1分钟后尝试重新链接");
+                    waiting(1);
+                } catch (Exception ex){
+                    log.error("下载任务出现异常，1分钟后尝试下一次下载",ex);
+                    waiting(1);
+                    ex.printStackTrace();
+                }
+            }
+        });
+        asynThread.start();
+    }
+
+
+    private void waiting(int minutes){
+        try { Thread.sleep(1000 * 60 * minutes);
+        } catch (InterruptedException e) {e.printStackTrace();}
+    }
+
+}
---
+++ achun-updown/achun-updown-app/src/main/java/site/achun/updown/app/service/module/download/DownloadTaskService.java
@@ -0,0 +1,178 @@
+package site.achun.updown.app.service.module.download;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.crypto.digest.MD5;
+import cn.hutool.http.HttpResponse;
+import com.alibaba.fastjson2.JSON;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import site.achun.file.client.enums.Type;
+import site.achun.file.client.module.download.constant.ResultStatus;
+import site.achun.file.client.module.download.request.Result;
+import site.achun.file.client.module.download.request.Task;
+import site.achun.file.client.module.file.FileUpdateClient;
+import site.achun.file.client.module.file.request.InitFileInfo;
+import site.achun.file.client.module.file.response.InitFileInfoResponse;
+import site.achun.file.client.module.storage.StorageQueryClient;
+import site.achun.file.client.module.storage.request.QueryStorageByCode;
+import site.achun.file.client.module.storage.response.StorageResponse;
+import site.achun.support.api.exception.RspException;
+import site.achun.updown.app.runner.DownloadTaskRunner;
+import site.achun.updown.app.service.module.transfer.FileTransferInfo;
+import site.achun.updown.app.service.module.transfer.FileTransferService;
+import site.achun.updown.app.util.HttpUtilPlus;
+
+import java.io.File;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DownloadTaskService {
+
+    public void handler(Task task){
+        log.info("Task:{}", JSON.toJSONString(task));
+    }
+
+    private final StorageQueryClient storageQueryClient;
+
+
+    private final FileUpdateClient fileUpdateClient;
+
+    private final FileTransferService fileTransferService;
+
+    public void tryDownload(Task task){
+        Result downloadRecord = null;
+        try{
+            // task 转换为DownloadRecord实体
+            downloadRecord = transfer(task);
+            // 下载AsynInfo实体
+            FileTransferInfo fileTransferInfo = download(task, downloadRecord);
+            // 对文件进行额外处理，比如：图片需要压缩，视频需要转码等
+            // 判断是否需要执行
+            if(needTransfer(downloadRecord)){
+                fileTransferService.transfer(fileTransferInfo);
+            }
+        }catch (RspException ex){
+            if(downloadRecord!=null){
+                downloadRecord.setStatus(ResultStatus.ERROR);
+                downloadRecord.setDescription(ex.getInfo());
+                downloadRecord.setMark(ex.getMark());
+            }
+            ex.printStackTrace();
+        }catch (Exception ex){
+            if(downloadRecord!=null){
+                downloadRecord.setStatus(ResultStatus.ERROR);
+                downloadRecord.setDescription(ex.getMessage());
+            }
+            ex.printStackTrace();
+        }
+        finally {
+            // 保存记录
+            try{
+//                downloadRecordConsumer.saveRecord(downloadRecord);
+                DownloadTaskRunner.WAITING=false;
+            }catch (Exception ex){
+                log.error("远程RPC接口调用失败，休息5分钟后，再次尝试调用...",ex);
+                ex.printStackTrace();
+                DownloadTaskRunner.WAITING=true;
+            }
+        }
+    }
+
+    private FileTransferInfo download(Task task, Result result){
+        // 判断文件是否存在，是否需要覆盖
+        long begin = System.currentTimeMillis();
+        FileTransferInfo fileTransferInfo = new FileTransferInfo();
+        StorageResponse storage = storageQueryClient.queryStorage(QueryStorageByCode.builder().code(task.getStorage()).build()).tryGetData();
+        result.setStorageLocalDir(storage.getPath());
+        fileTransferInfo.setStorage(storage);
+
+        // 判断是否已经存在本地文件，是否需要覆盖
+        String localFilePath = storage.getPath() + task.getKey();
+        File newFile = new File(localFilePath);
+        fileTransferInfo.setFile(newFile);
+        if(newFile.exists()){
+            result.setExistsLocalFile(true);
+            if(task.getOverride()){
+                newFile.delete();
+                log.info("exists file:{}, will be override",newFile.getAbsolutePath());
+            }else{
+                log.info("exists file:{},no override",newFile.getAbsolutePath());
+                result.setStatus(ResultStatus.SUCCESS_NO_OVERRIDE);
+                return fileTransferInfo;
+            }
+        }
+
+        // 开始下载文件
+        HttpResponse response = HttpUtilPlus.downloadFile(
+                task.getUrl(),
+                newFile,
+                -1,
+                null,
+                null);
+        result.setHttpStatus(response.getStatus());
+
+        if(response.isOk()){
+            // 记录日志
+            InitFileInfoResponse fileResponse = fileUpdateClient.initFileInfo(transfer(newFile, task)).tryGetData();
+            fileTransferInfo.setFileCode(fileResponse.getFileCode());
+            fileTransferInfo.setInStoragePath(fileResponse.getInStoragePath());
+            // 设置参数
+            result.setFileCode(fileResponse.getFileCode());
+            result.setFileSize(fileResponse.getSize());
+            Long cost = System.currentTimeMillis() - begin;
+            if(result.getExistsLocalFile()!=null && result.getExistsLocalFile())
+                result.setStatus(ResultStatus.SUCCESS_OVERRIDE);
+            else
+                result.setStatus(ResultStatus.SUCCESS);
+            result.setCostTime(cost.intValue());
+            log.info("download:{},size:{},cost:{}",newFile.getAbsolutePath(),newFile.length(),cost);
+        }else{
+            log.error("download asynTask Error,httpStatus[{}],asynTask:{},response:{}",
+                    response.getStatus(),task,response);
+        }
+        return fileTransferInfo;
+    }
+
+    private InitFileInfo transfer(File file, Task task){
+        String suffix = FileUtil.getSuffix(file);
+        String md5 = MD5.create().digestHex(file);
+        String fileCode = MD5.create().digestHex(md5+task.getUnit());
+        InitFileInfo info = InitFileInfo.builder()
+                .fileCode(fileCode)
+                .thirdId(task.getThirdId())
+                .md5(md5)
+                .storageCode(task.getStorage())
+                .fileName(file.getName())
+                .absolutePath(file.getAbsolutePath())
+                .unitCode(task.getUnit())
+                .size(file.length()/1024)
+                .suffix(suffix)
+                .type(Type.parse(suffix).getCode())
+                .build();
+        return info;
+    }
+
+    private Result transfer(Task task){
+        return Result.builder()
+                .override(task.getOverride())
+                .storage(task.getStorage())
+                .fileCode(task.getFileCode())
+                .unit(task.getUnit())
+                .filePath(task.getKey())
+                .existsLocalFile(false)
+                .url(task.getUrl())
+                .build();
+    }
+
+    // 判断是否满足文件转换条件
+    private boolean needTransfer(Result result){
+        // 如果下载成功
+        if(result.getStatus().equals(ResultStatus.SUCCESS)) return true;
+        // 如果成功覆盖
+        if(result.getStatus().equals(ResultStatus.SUCCESS_OVERRIDE)) return true;
+        return false;
+    }
+
+}
