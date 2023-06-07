fd4eda343ada11050c74a4f416164164d21ac71b
Heiffeng <blacard@163.com>
2023-06-07 18:41:54 +0800

    接口实现

---
+++ achun-gallery/achun-gallery-app/src/main/java/site/achun/gallery/app/controller/album_record/AlbumRecordUpdateController.java
@@ -0,0 +1,61 @@
+package site.achun.gallery.app.controller.album_record;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+import site.achun.gallery.app.service.album_record.AlbumRecordMoveService;
+import site.achun.gallery.app.service.pictures.UploadPictureService;
+import site.achun.gallery.app.service.unit.AnewFileSetService;
+import site.achun.gallery.app.utils.UserInfo;
+import site.achun.gallery.client.module.album.request.MoveAlbumRecordRequest;
+import site.achun.gallery.client.module.album_record.AlbumRecordUpdateClient;
+import site.achun.gallery.client.module.album_record.request.UploadPictures;
+import site.achun.gallery.client.module.fileset.request.AnewFileSet;
+import site.achun.gallery.client.module.fileset.response.FileSetResponse;
+import site.achun.gallery.client.module.pictures.request.UploadPicturesRequest;
+import site.achun.support.api.response.Rsp;
+
+import java.util.Collection;
+
+@Slf4j
+@Tag(name = "相册记录更新")
+@RestController
+@RequiredArgsConstructor
+public class AlbumRecordUpdateController implements AlbumRecordUpdateClient {
+
+    private final UploadPictureService uploadPictureService;
+    private final AnewFileSetService anewFileSetService;
+    private final AlbumRecordMoveService albumRecordMoveService;
+    @Override
+    public Rsp<FileSetResponse> uploadPictures(UploadPictures createInfo) {
+        createInfo.setUserCode(UserInfo.getCode(createInfo::getUserCode));
+        UploadPicturesRequest createFileSet = UploadPicturesRequest.builder()
+                .setCode(createInfo.getSetCode())
+                .albumCode(createInfo.getAlbumCode())
+                .tags(createInfo.getTags())
+                .name(createInfo.getName())
+                .desc(createInfo.getDesc())
+                .userCode(createInfo.getUserCode())
+                .build();
+        return uploadPictureService.createFileSet(createFileSet);
+    }
+
+    @Override
+    public Rsp<Boolean> anewFileSet(AnewFileSet anewGroup) {
+        anewFileSetService.anewFileset(anewGroup);
+        return Rsp.success(true);
+    }
+
+    @Override
+    public Rsp<Boolean> moveRecords(MoveAlbumRecordRequest req) {
+        albumRecordMoveService.move(req);
+        return Rsp.success(true);
+    }
+
+    @Override
+    public Rsp<Boolean> deleteBatchRecords(Collection<String> fileCodes) {
+        // TODO 文件的也要删除。暂停下。
+        return null;
+    }
+}
---
+++ achun-gallery/achun-gallery-client/src/main/java/site/achun/gallery/client/module/album_record/AlbumRecordUpdateClient.java
@@ -0,0 +1,34 @@
+package site.achun.gallery.client.module.album_record;
+
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import site.achun.gallery.client.module.album.request.MoveAlbumRecordRequest;
+import site.achun.gallery.client.module.album_record.request.UploadPictures;
+import site.achun.gallery.client.module.fileset.request.AnewFileSet;
+import site.achun.gallery.client.module.fileset.response.FileSetResponse;
+import site.achun.support.api.response.Rsp;
+
+import java.util.Collection;
+
+@FeignClient(name = "achun-gallery-app", contextId = "AlbumRecordUpdateClient")
+public interface AlbumRecordUpdateClient {
+
+    @Operation(summary = "创建相册记录,上传图片")
+    @PostMapping("/gallery/album-record/upload-pictures")
+    public Rsp<FileSetResponse> uploadPictures(@RequestBody UploadPictures createInfo);
+
+    @Operation(summary = "重新分组")
+    @PostMapping("/gallery/album-record/anew-fileset")
+    public Rsp<Boolean> anewFileSet(@RequestBody @Valid AnewFileSet anewGroup);
+
+    @Operation(summary = "移动一个相册内文件组，到另一个相册。")
+    @PostMapping("/gallery/album-record/move")
+    public Rsp<Boolean> moveRecords(@RequestBody MoveAlbumRecordRequest req);
+
+    @Operation(summary = "批量删除文件")
+    @PostMapping("/gallery/album-record/batch-delete")
+    public Rsp<Boolean> deleteBatchRecords(@RequestBody Collection<String> fileCodes);
+}
