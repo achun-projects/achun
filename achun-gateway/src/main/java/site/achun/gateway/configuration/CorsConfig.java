18b8cb0e5e28c6e9ab409e0abf940cd531e1226f
Heiffeng <suna235792@gmail.com>
2023-06-08 16:28:46 +0800

    Update CorsConfig.java

--- achun-gateway/src/main/java/site/achun/gateway/configuration/CorsConfig.java
+++ achun-gateway/src/main/java/site/achun/gateway/configuration/CorsConfig.java
@@ -7,6 +7,8 @@ import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.util.pattern.PathPatternParser;
 
+import java.util.Arrays;
+
 /**
  * Author: Heiffeng
  * Date: 2023/3/30
@@ -15,12 +17,14 @@ import org.springframework.web.util.pattern.PathPatternParser;
 public class CorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource (new PathPatternParser());
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
         CorsConfiguration corsConfig = new CorsConfiguration();
         // 允许所有请求方法
         corsConfig.addAllowedMethod("*");
-        // 允许所有域，当请求头
-        corsConfig.addAllowedOrigin("*");
+        // 允许指定的域，可以设置多个来源
+//        corsConfig.addAllowedOrigin("http://example1.com");
+//        corsConfig.addAllowedOrigin("http://example2.com");
+        corsConfig.setAllowedOriginPatterns(Arrays.asList("http://*.achun.site"));
         // 允许全部请求头
         corsConfig.addAllowedHeader("*");
         // 允许携带 Cookie 等用户凭证
@@ -30,4 +34,6 @@ public class CorsConfig {
 
         return new CorsWebFilter(source);
     }
+
+
 }
