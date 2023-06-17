09f59d755eea6ba05033842db2321e1780511a7d
Heiffeng <suna235792@gmail.com>
2023-06-17 15:02:38 +0800

    删除文件

--- achun-updown/achun-updown-app/src/main/java/site/achun/updown/app/rabbitmq/FileRemoveListener.java
+++ achun-updown/achun-updown-app/src/main/java/site/achun/updown/app/rabbitmq/FileRemoveListener.java
@@ -4,26 +4,19 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
-import site.achun.file.client.module.file.FileQueryClient;
-import site.achun.file.client.module.file.request.QueryByFileCode;
-import site.achun.file.client.module.file.response.FileInfoResponse;
-import site.achun.file.client.module.file.response.MediaFileResponse;
+import site.achun.updown.app.service.module.delete.FileDeleteService;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class FileRemoveListener {
 
-    private final FileQueryClient fileQueryClient;
+    private final FileDeleteService fileDeleteService;
 
     @RabbitListener(queues = "file.remove.queue")
     public void whenFileUpdate(String fileCode){
         log.info("fileRemove :{}",fileCode);
-//        FileInfoResponse file = fileQueryClient.queryFile(QueryByFileCode.builder().fileCode(fileCode).build()).getData();
-//        if(!LISTEN_BUCKETS.contains(file.getBucketCode())){
-//            log.info("fileNotIn,fileCode:{},bucketCode:{}",fileCode,file.getBucketCode());
-//        }
-//        picturesUpdateService.update(file);
+        fileDeleteService.deleteFile(fileCode);
     }
 
 }
---
+++ achun-updown/achun-updown-app/src/main/java/site/achun/updown/app/service/module/delete/FileDeleteService.java
@@ -0,0 +1,50 @@
+package site.achun.updown.app.service.module.delete;
+
+import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson2.JSON;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import site.achun.file.client.module.file.FileQueryClient;
+import site.achun.file.client.module.file.FileUpdateClient;
+import site.achun.file.client.module.file.request.DeleteFileRequest;
+import site.achun.file.client.module.file.request.QueryByFileCode;
+import site.achun.file.client.module.file.response.FileLocalInfoResponse;
+import site.achun.support.api.response.Rsp;
+
+import java.io.File;
+import java.nio.file.Path;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class FileDeleteService {
+
+    private final FileQueryClient fileQueryClient;
+    private final FileUpdateClient fileUpdateClient;
+
+    public void deleteFile(String fileCode){
+        if(StrUtil.isBlank(fileCode)){
+            log.info("deleteFile fileCode is Blank");
+            return;
+        }
+        Rsp<FileLocalInfoResponse> fileInfoRsp = fileQueryClient.queryFileLocalInfo(QueryByFileCode.builder().fileCode(fileCode).build());
+        if(!fileInfoRsp.hasData()){
+            log.info("deleteFile fileCode查询不到数据，fileCode:{},fileInfoRsp:{}",fileCode, JSON.toJSONString(fileInfoRsp));
+            return;
+        }
+        FileLocalInfoResponse localFileInfo = fileInfoRsp.getData();
+        File file = Path.of(localFileInfo.getStorage().getPath(),localFileInfo.getInStoragePath()).toFile();
+        File smallFile = Path.of(localFileInfo.getStorage().getPath(),"_small",localFileInfo.getInStoragePath()).toFile();
+        File mediumFile = Path.of(localFileInfo.getStorage().getPath(),"_medium",localFileInfo.getInStoragePath()).toFile();
+        log.info("file:{} {}",file.getAbsolutePath(),file.exists());
+        log.info("smallFile:{} {}",smallFile.getAbsolutePath(),smallFile.exists());
+        log.info("mediumFile:{} {}",mediumFile.getAbsolutePath(),mediumFile.exists());
+        if(file.exists()) file.delete();
+        if(smallFile.exists()) smallFile.delete();
+        if(mediumFile.exists()) mediumFile.delete();
+
+        Boolean result = fileUpdateClient.realDeleteFileInfo(DeleteFileRequest.builder().fileCode(fileCode).build()).getData();
+        log.info("待删除文件：{},删除结果：{}",fileCode,result);
+    }
+}
