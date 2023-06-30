d7bf6bcd31435069be9165ba6dec664db731f70f
Heiffeng <suna235792@gmail.com>
2023-06-30 18:46:54 +0800

    升级jar包

--- achun-file/achun-file-client/src/main/java/site/achun/file/client/configuration/FileFeignConfiguration.java
+++ achun-file/achun-file-client/src/main/java/site/achun/file/client/configuration/FileFeignConfiguration.java
@@ -2,20 +2,17 @@ package site.achun.file.client.configuration;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.util.StringUtils;
-import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
-import javax.servlet.http.HttpServletRequest;
 
 public class FileFeignConfiguration implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate template) {
         // 从header获取X-token
-        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
-        ServletRequestAttributes attr = (ServletRequestAttributes) requestAttributes;
-        HttpServletRequest request = attr.getRequest();
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
         String env = request.getHeader("env");//网关传过来的 token
         if (StringUtils.hasText(env)) {
             template.header("env", env);
