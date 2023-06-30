6f74496886797fe818fb17c9eafeec4e57504404
Heiffeng <suna235792@gmail.com>
2023-06-30 14:13:11 +0800

    更新相册访问时间

--- achun-gallery/achun-gallery-app/src/main/java/site/achun/gallery/app/listener/UnitUpdateListener.java
+++ achun-gallery/achun-gallery-app/src/main/java/site/achun/gallery/app/listener/UnitUpdateListener.java
@@ -17,7 +17,7 @@ public class UnitUpdateListener {
 
     @RabbitListener(queues = "unit.update.queue")
     public void whenUnitUpdate(String msg){
-        log.info("fileUpdate :{}",msg);
+        log.info("unitUpdate :{}",msg);
         UpdateUnit update = JSON.parseObject(msg,UpdateUnit.class);
         fileUnitUpdateService.updateUnit(update);
     }
--- achun-gallery/achun-gallery-app/src/main/java/site/achun/gallery/app/service/pictures/UploadPictureService.java
+++ achun-gallery/achun-gallery-app/src/main/java/site/achun/gallery/app/service/pictures/UploadPictureService.java
@@ -3,6 +3,7 @@ package site.achun.gallery.app.service.pictures;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import site.achun.gallery.app.generator.service.AlbumService;
 import site.achun.gallery.app.service.album_record.AlbumRecordUpdateExecute;
 import site.achun.gallery.app.service.unit.PicUnitQueryExecute;
 import site.achun.gallery.app.service.unit.PicUnitUpdateExecute;
@@ -16,6 +17,7 @@ import site.achun.support.api.response.Rsp;
 @RequiredArgsConstructor
 public class UploadPictureService{
 
+    private final AlbumService albumService;
     private final PicUnitUpdateExecute fileSetUpdateExecute;
     private final PicUnitQueryExecute fileSetQueryExecute;
     private final AlbumRecordUpdateExecute albumRecordUpdateExecute;
@@ -32,6 +34,8 @@ public class UploadPictureService{
         );
         // 创建相册记录
         albumRecordUpdateExecute.createAlbumRecord(createFileSet.getAlbumCode(),createFileSet.getSetCode());
+        // 更新相册访问时间
+        albumService.updateRecordUtime(createFileSet.getAlbumCode());
         return Rsp.success(fileSetQueryExecute.queryByUnitCode(createFileSet.getSetCode()));
     }
 }
