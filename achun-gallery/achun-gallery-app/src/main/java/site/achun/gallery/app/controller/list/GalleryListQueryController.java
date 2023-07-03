6864373c8fb16d1d739e2ea651985e5d4a414077
Heiffeng <suna235792@gmail.com>
2023-07-03 11:52:05 +0800

    从列表随机查

--- achun-gallery/achun-gallery-app/src/main/java/site/achun/gallery/app/controller/list/GalleryListQueryController.java
+++ achun-gallery/achun-gallery-app/src/main/java/site/achun/gallery/app/controller/list/GalleryListQueryController.java
@@ -6,6 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 import site.achun.gallery.app.service.list.ListRandomQueryService;
 import site.achun.gallery.client.module.list.GalleryListQueryClient;
+import site.achun.gallery.client.module.list.request.RandomQueryFromList;
+import site.achun.gallery.client.module.pictures.response.Photo;
+import site.achun.support.api.response.Rsp;
 
 import java.util.Collection;
 
@@ -20,6 +23,15 @@ public class GalleryListQueryController implements GalleryListQueryClient {
     @Override
     public String randomQuery(Collection<String> listCodes) {
         return listRandomQueryService.randomQuery(listCodes);
+    }
+
+    @Override
+    public Rsp<Photo> randomQueryOnePhoto(RandomQueryFromList query) {
+        Photo photo = listRandomQueryService.randomQueryOnePhoto(query.getListCode());
+        if(photo == null){
+            return Rsp.error("Not found");
+        }
+        return Rsp.success(photo);
     }
 
 
--- achun-gallery/achun-gallery-client/src/main/java/site/achun/gallery/client/module/list/GalleryListQueryClient.java
+++ achun-gallery/achun-gallery-client/src/main/java/site/achun/gallery/client/module/list/GalleryListQueryClient.java
@@ -3,6 +3,10 @@ package site.achun.gallery.client.module.list;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import site.achun.gallery.client.module.list.request.RandomQueryFromList;
+import site.achun.gallery.client.module.pictures.response.Photo;
+import site.achun.support.api.response.Rsp;
 
 import java.util.Collection;
 
@@ -13,5 +17,9 @@ public interface GalleryListQueryClient {
     @PostMapping("/gallery/list/random-query")
     @Deprecated
     String randomQuery(Collection<String> listCodes);
+
+    @Operation(summary = "随机从列表查询一张图")
+    @PostMapping("/gallery/list/random-query-one-photo-from-list")
+    Rsp<Photo> randomQueryOnePhoto(@RequestBody RandomQueryFromList query);
 
 }
