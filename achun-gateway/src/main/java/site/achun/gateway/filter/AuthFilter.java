a2f0653326a07bde8aff19a9b0e11197e2340d12
Heiffeng <suna235792@gmail.com>
2023-07-26 17:56:58 +0800

    完善权限过滤

--- achun-gateway/src/main/java/site/achun/gateway/filter/AuthFilter.java
+++ achun-gateway/src/main/java/site/achun/gateway/filter/AuthFilter.java
@@ -1,35 +1,48 @@
 package site.achun.gateway.filter;
 
+import com.alibaba.fastjson2.JSON;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.context.ApplicationContext;
 import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 import site.achun.gateway.service.UserLoginService;
+import site.achun.support.api.response.Rsp;
+import site.achun.user.client.module.login.dto.UserCacheInfo;
+
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class AuthFilter implements GatewayFilter, Ordered {
     private static final String AUTH_TOKEN = "Authorization";
 
-    private final StringRedisTemplate redisTemplate;
-    private final static String KEY = "USER:LOGIN:%s";
+    private final UserLoginService userLoginService;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
         ServerHttpResponse response = exchange.getResponse();
         String satoken = request.getHeaders().getFirst("satoken");
-        String userCode = redisTemplate.opsForValue().get(String.format(KEY, satoken));
+        UserCacheInfo userCacheInfo = userLoginService.getByToken(satoken);
+        if(userCacheInfo==null){
+            byte[] bits = JSON.toJSONString(Rsp.error("无效token")).getBytes(StandardCharsets.UTF_8);
+            DataBuffer buffer = response.bufferFactory().wrap(bits);
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            //指定编码，否则在浏览器中会中文乱码
+            response.getHeaders().add("Content-Type", "text/plain;charset=UTF-8");
+            return response.writeWith(Mono.just(buffer));
+        }
+        String userCode = userCacheInfo.getUserCode();
         // 在请求属性中设置header
         request = request.mutate()
                 .header("user-code", userCode)
@@ -38,7 +51,6 @@ public class AuthFilter implements GatewayFilter, Ordered {
         // 如果校验不通过，可以返回一个错误响应，例如：
         // response.setStatusCode(HttpStatus.UNAUTHORIZED);
         // return response.setComplete();
-        log.info("pass auth filter");
         // 校验通过则继续向下传递
         return chain.filter(exchange.mutate().request(request).build());
     }
--- achun-gateway/src/main/java/site/achun/gateway/service/UserLoginService.java
+++ achun-gateway/src/main/java/site/achun/gateway/service/UserLoginService.java
@@ -1,40 +1,63 @@
 package site.achun.gateway.service;
 
+import com.alibaba.fastjson2.JSON;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
-import site.achun.support.api.response.Rsp;
-import site.achun.user.client.module.login.UserLoginClient;
-import site.achun.user.client.module.login.response.LoginResponse;
+import org.springframework.util.CollectionUtils;
+import site.achun.user.client.module.login.dto.UserCacheInfo;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
 @AllArgsConstructor
 public class UserLoginService {
 
-    private final ApplicationContext applicationContext;
+    private final static String KEY = "USER:LOGIN:%s:%s";
+    private final StringRedisTemplate redisTemplate;
 
-    public String getUserCode(String satoken){
-        UserLoginClient userLoginClient = applicationContext.getBean(UserLoginClient.class);
-        CompletableFuture<Rsp<LoginResponse>> f = CompletableFuture.supplyAsync(()->{
-               return userLoginClient.checkToken(satoken);
-        });
-        Rsp<LoginResponse> loginResponse = null;
-        try {
-            loginResponse = f.get();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
+    private final static Long DEFAULT_TIMEOUT = 60 * 60 * 24 * 2L; // 2天
+
+    /**
+     * 设置缓存
+     * @param token token
+     * @param userCode 用户编码
+     * @param timeout 失效时间，单位秒
+     */
+    public void put(String token,String userCode,Long timeout){
+        if(timeout==null || timeout <= 0){
+            timeout = DEFAULT_TIMEOUT;
         }
-        if(loginResponse.hasData()){
-            return loginResponse.getData().getUserCode();
-        }else{
-            return "1";
-        }
+        String key = String.format(KEY,token,userCode);
+        UserCacheInfo info = UserCacheInfo.builder()
+                .timeout(timeout)
+                .token(token)
+                .userCode(userCode)
+                .build();
+        redisTemplate.opsForValue().set(key, JSON.toJSONString(info),timeout, TimeUnit.SECONDS);
     }
+
+    public UserCacheInfo getByToken(String token){
+        String patternKey = String.format(KEY,token,"*");
+        return getByPatternKey(patternKey);
+    }
+
+    public UserCacheInfo getByUserCode(String userCode){
+        String patternKey = String.format(KEY,"*",userCode);
+        return getByPatternKey(patternKey);
+    }
+
+    public UserCacheInfo getByPatternKey(String patternKey){
+        Set<String> keys = redisTemplate.keys(patternKey);
+        if(CollectionUtils.isEmpty(keys)){
+            return null;
+        }
+        String key = keys.stream().findFirst().get();
+        String json = redisTemplate.opsForValue().get(key);
+        return JSON.parseObject(json,UserCacheInfo.class);
+    }
+
 }
