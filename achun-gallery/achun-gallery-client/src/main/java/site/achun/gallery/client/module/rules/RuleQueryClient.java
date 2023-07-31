ae9669160135e2886312dd1ae08ef395e217af3a
Heiffeng <suna235792@gmail.com>
2023-07-31 16:50:01 +0800

    Update RuleQueryClient.java

--- achun-gallery/achun-gallery-client/src/main/java/site/achun/gallery/client/module/rules/RuleQueryClient.java
+++ achun-gallery/achun-gallery-client/src/main/java/site/achun/gallery/client/module/rules/RuleQueryClient.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import site.achun.file.client.configuration.FileFeignConfiguration;
 import site.achun.file.client.module.file.response.MediaFileResponse;
 import site.achun.gallery.client.module.pictures.response.PicturesBasicInfo;
 import site.achun.gallery.client.module.rules.requset.QueryFileByRuleCode;
@@ -11,7 +12,7 @@ import site.achun.support.api.response.Rsp;
 
 import java.util.List;
 
-@FeignClient(name = "achun-gallery-app", contextId = "RuleQueryClient")
+@FeignClient(name = "achun-gallery-app", contextId = "RuleQueryClient",configuration = {FileFeignConfiguration.class})
 public interface RuleQueryClient {
     @Operation(summary = "根据规则编码查询一组文件")
     @PostMapping("/gallery/rules/query-files-by-rule-code")
