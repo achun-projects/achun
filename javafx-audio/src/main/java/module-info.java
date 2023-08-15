d40ee11560f6d967eea59ca3dbe08aa8e1c1fdae
Heiffeng <suna235792@gmail.com>
2023-08-15 16:31:49 +0800

    添加javafx-audio项目

---
+++ javafx-audio/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module site.achun.audio.javafx.javafxaudio {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+                requires org.kordamp.ikonli.javafx;
+            requires org.kordamp.bootstrapfx.core;
+            requires atlantafx.base;
+            
+    opens site.achun.audio.javafx.javafxaudio to javafx.fxml;
+    exports site.achun.audio.javafx.javafxaudio;
+}
---
+++ javafx-audio/src/main/java/site/achun/audio/javafx/javafxaudio/HelloApplication.java
@@ -0,0 +1,28 @@
+package site.achun.audio.javafx.javafxaudio;
+
+import atlantafx.base.theme.PrimerDark;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        // find more themes in 'atlantafx.base.theme' package
+        Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
