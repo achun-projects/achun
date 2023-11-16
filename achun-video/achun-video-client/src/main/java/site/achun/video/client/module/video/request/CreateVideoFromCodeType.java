920ec1b9d17c7a03abfe4f1918f5d4df5b9e705f
Heiffeng <suna235792@gmail.com>
2023-11-16 16:34:06 +0800

    Update CreateVideoFromCodeType.java

--- achun-video/achun-video-client/src/main/java/site/achun/video/client/module/video/request/CreateVideoFromCodeType.java
+++ achun-video/achun-video-client/src/main/java/site/achun/video/client/module/video/request/CreateVideoFromCodeType.java
@@ -4,20 +4,22 @@ import com.baomidou.mybatisplus.annotation.EnumValue;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import site.achun.support.api.enums.BaseEnum;
 
 @Getter
 @AllArgsConstructor
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
-public enum CreateVideoFromCodeType {
+public enum CreateVideoFromCodeType implements BaseEnum {
     DIRS(1),
     FILES(2),
     PARENT_DIR(3);
 
 
     @EnumValue
-    private Integer type;
+    private Integer code;
 
     public Integer getType() {
-        return type;
+        return code;
     }
+
 }
